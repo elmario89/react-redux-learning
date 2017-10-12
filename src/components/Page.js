0d05b3b12348c4faf51bbd277708a9c760445924
Smirnov Mikhail <urine89@mail.ru>
2017-10-13 00:43:06 +0300

    add action to change year, add constants

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Page extends Component {
+    onYearBtnClick(e) {
+        console.log(this.props)
+        this.props.setYear(+e.target.textContent)
+    }
     render() {
         const { year, photos } = this.props
         return <div>
+            <p>
+                <button onClick={::this.onYearBtnClick}>2016</button>
+                <button onClick={::this.onYearBtnClick}>2015</button>
+                <button onClick={::this.onYearBtnClick}>2014</button>
+            </p>
             You have {photos.length} photos in {year} year!
         </div>
     }
