216ce2e41ff13ec012095f9ea2d7f43d79361870
Smirnov Mikhail <urine89@mail.ru>
2017-10-15 18:06:45 +0300

    add fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
         'babel-polyfill',
+        'whatwg-fetch',
 	    'react-hot-loader/patch',
 		'webpack-hot-middleware/client',
 		'./src/index',
